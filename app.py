621c0adadc4edb8f687441f791342409903b7c69
Rohan Gandhi <rohangandhi109@gmail.com>
2020-12-31 01:14:36 -0500

    'deploy'

--- app.py
+++ app.py
@@ -25,11 +25,11 @@ def after_request(response):
 
 auth0 = oauth.register(
     'auth0',
-    client_id = os.environ['AUTH0_CLIENT_ID'],
-    client_secret = os.environ['AUTH0_CLIENT_SECRET'],
-    api_base_url = os.environ['AUTH0_BASE_URL'],
-    access_token_url =os.environ['AUTH0_BASE_URL'] + '/oauth/token',
-    authorize_url = os.environ['AUTH0_BASE_URL'] + '/authorize',
+    client_id = AUTH0_CLIENT_ID,
+    client_secret = AUTH0_CLIENT_SECRET,
+    api_base_url = AUTH0_BASE_URL,
+    access_token_url =AUTH0_BASE_URL + '/oauth/token',
+    authorize_url = AUTH0_BASE_URL + '/authorize',
     client_kwargs={
         'scope': 'openid profile email',
     },
@@ -48,7 +48,7 @@ from controllers import userController, developer,ticket,comment,project,Admin,m
 
 @app.route('/')
 def login():
-    return auth0.authorize_redirect(redirect_uri=os.environ['AUTH0_CALLBACK_URL'], audience=os.environ['AUTH0_AUDIENCE'])
+    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE)
 
 @app.route('/callback')
 def callback_handling():
@@ -76,7 +76,7 @@ def temp():
 @app.route('/logout')
 def logout():
     session.clear()
-    params = {'returnTo': url_for('login', _external=True), 'client_id': os.environ['AUTH0_CLIENT_ID']}
+    params = {'returnTo': url_for('login', _external=True), 'client_id': AUTH0_CLIENT_ID}
     return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
 
--- info.py
+++ info.py
@@ -1,4 +1,12 @@
 from datetime import date
+# AUTH0_CALLBACK_URL = 'http://127.0.0.1:5000/callback'
+AUTH0_CALLBACK_URL = 'http://bugtracker109-stage.herokuapp.com/callback'
+AUTH0_CLIENT_ID = '2Qo9NMZBSqfdIvmx5Oeh2v0AGTKL61bB'
+AUTH0_CLIENT_SECRET = 'YoMyvqDqWOP9IAWSv1HwQ_vjnK5wK_tJFDhRd0X39vkOy_Vfq7O78G84BWduc7nJ'
+AUTH0_DOMAIN = 'dev-9oonecyt.us.auth0.com'
+AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_AUDIENCE = 'bugTracker'
+AUTH0_ALGORITHM = ['RS256']
 PRIORITY=['High','Low','Medium']
 STATUS=['open','closed','in-progress']
 DATE=date.today().strftime("%d/%m/%Y")
